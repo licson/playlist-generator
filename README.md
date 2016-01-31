4592aa358acf3940bd7816f53051effc6e429000
Licson Lee <licson0729@gmail.com>
2016-01-31 18:03:18 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Playlist Generator
+====================
+
+This is a HTML music playlist generator with simplicity in mind.
+
+Usage
+====================
+
+1. Clone this repository
+2. Put some MP3s in the folder
+3. `npm install`
+4. `./generate.js -a "Album name" -c "Composer"`
+5. Open `index.html` to see the playlist
+6. Upload `index.html`, `play-button.js` and all your music file to some web host _(Optional)_
+
+Development
+====================
+
+Want to change how the playlist looks? Checkout `template.mustache` for some details.
+
+License
+====================
+
+MIT
---
+++ generate.js
@@ -0,0 +1,70 @@
+#!/usr/bin/node
+
+var fs = require('fs');
+var mu = require('mu2');
+var program = require('commander');
+
+// Setup template location
+mu.root = __dirname;
+
+var generate = function(album, composer){
+	console.log('Generating playlist for %s - %s', album, composer);
+	
+	fs.readdir(__dirname, function(e, result){
+		if(e){
+			console.error('Something went wrong!');
+			return;
+		}
+
+		var output = [];
+		result.forEach(function(item, i){
+			var ext = item.split('.').pop().toLowerCase();
+			var name = item.replace('.' + ext, '');
+			var needConversion = ext != 'mp3' ? true : false;
+
+			if(!needConversion){
+				if(ext !== item){
+					output.push({
+						file: name,
+						src: encodeURI(item),
+					});
+					
+					console.warn('Added ' + item);
+				}
+			}
+			else {
+				console.warn('File type not supported, skipping!');
+			}
+		});
+
+		output.sort(function(a, b){
+			var x = a.file.toLowerCase();
+			var y = b.file.toLowerCase();
+			return x < y ? -1 : x > y ? 1 : 0;
+		});
+
+		mu.compileAndRender('template.mustache', {
+			name: album,
+			composer: composer,
+			srcset: output
+		}).pipe(fs.createWriteStream(__dirname + '/index.html'));
+	});
+}
+
+var album = 'Untitled Album', composer = 'Unknown Composer';
+
+program
+	.version('1.0.0')
+	.option('-a, --album [album]', 'Album name')
+	.option('-c, --composer [composer]', 'Composer')
+	.parse(process.argv);
+
+if(program.album){
+	album = program.album;
+}
+
+if(program.album){
+	composer = program.composer;
+}
+
+generate(album, composer);
